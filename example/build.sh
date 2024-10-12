4ac12a1366498d46e59ae1207ba071ae63a8c9f4
Ott Boonpirom <ottboonpirom@gmail.com>
2024-10-11 22:05:52 -0500

    remove redundant compile flags

--- example/build.sh
+++ example/build.sh
@@ -3,11 +3,11 @@ case $1 in
     "" | "windows")
         splat_dir=~/.local/share/Geode/cross-tools/splat
         toolchain=~/.local/share/Geode/cross-tools/clang-msvc-sdk/clang-cl-msvc.cmake
-        geode build -p windows -- -DCMAKE_TOOLCHAIN_FILE=$toolchain -DSPLAT_DIR=$splat_dir -DRust_CARGO_TARGET=x86_64-pc-windows-msvc ;;
+        geode build -p windows -- -DCMAKE_TOOLCHAIN_FILE=$toolchain -DSPLAT_DIR=$splat_dir ;;
     "android" | "android32" | "android64")
         if [[ -z $ANDROID_NDK_ROOT ]]; then
             export ANDROID_NDK_ROOT=~/.local/share/Geode/cross-tools/android-ndk
-        fi;&
+        fi ;&
     *)
         geode build -p $1 --config RelWithDebInfo ;;
 esac
